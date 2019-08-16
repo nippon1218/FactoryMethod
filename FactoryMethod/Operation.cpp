74f103ba973b26fdecaab6bca1dbfd025876ff0f
VincentChen <nippon1218@163.com>
2019-08-16 23:14:27 +0800

    创建FactoryMethod方法

---
+++ FactoryMethod/Operation.cpp
@@ -0,0 +1,21 @@
+//
+//  Operation.cpp
+//  FactoryMethod
+//
+//  Created by Vincent on 2019/8/16.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#include "Operation.hpp"
+
+class Operation
+{
+public:
+    double _numberA = 0;
+    double _numberB = 0;
+    
+    virtual double getResult(float numA, float numB) = 0;
+};
+
+
+
---
+++ FactoryMethod/Operation.hpp
@@ -0,0 +1,14 @@
+//
+//  Operation.hpp
+//  FactoryMethod
+//
+//  Created by Vincent on 2019/8/16.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#ifndef Operation_hpp
+#define Operation_hpp
+
+#include <stdio.h>
+
+#endif /* Operation_hpp */
