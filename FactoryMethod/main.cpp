b78cf5dfcc4edb8341a078c9c756313a25628fec
VincentChen <nippon1218@163.com>
2019-08-16 22:26:46 +0800

    Initial Commit

---
+++ FactoryMethod/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FactoryMethod
+//
+//  Created by Vincent on 2019/8/16.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
